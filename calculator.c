6b6f1962885e9870318ea52b9a7f4e41be1ff4f2
SampreetyPillai <110045487+SampreetyPillai@users.noreply.github.com>
2022-07-27 00:02:45 +0530

    Taking input from user

---
+++ calculator.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+int main(){
+
+    float x,y;
+    printf("Enter first number: ");
+    scanf("%f",&x);
+    
+    printf("Enter second number: ");
+    scanf("%f",&y);
+    printf("Which operation would you like to perform?\nAddition( press 1 )\nSubtraction( press 2)\nMultiplication( press 3)\nDivision( press 4)\n");
+    int num;
+    
+    
+
+    
+    
+
+
+    return 0;
+}

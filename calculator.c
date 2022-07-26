170b19f5ffa2517948844a8aee70b72fcac107c8
SampreetyPillai <110045487+SampreetyPillai@users.noreply.github.com>
2022-07-27 00:13:26 +0530

    Division operator
    Additional check to see if number was divided by 0

--- calculator.c
+++ calculator.c
@@ -11,23 +11,34 @@ int main(){
     scanf("%f",&y);
     printf("Which operation would you like to perform?\nAddition( press 1 )\nSubtraction( press 2)\nMultiplication( press 3)\nDivision( press 4)\n");
     int num;
+    scanf("%d", &num);
+
+    if (num==4 && y==0){
+        printf("Cannot divide by 0");
+    }else{
     
     float new;
-    scanf("%d", &num);
+    
     switch(num){
         case 1:
         new = x+y;
         break;
-            
+
         case 2:
         new = x-y;
         break;
-            
+
         case 3:
         new = x*y;
         break;
+        
+        case 4:
+        new = x/y;
+        break;
     }
     printf("Result is: %f", new);
+    }
+    
 
     return 0;
 }
